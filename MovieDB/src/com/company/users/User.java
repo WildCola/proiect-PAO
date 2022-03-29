74a227588818daf7be0328a439d5968c6eb22b6c
WildCola <stefan.placintescu@s.unibuc.ro>
2022-03-29 11:58:28 +0300

    Update User.java

--- MovieDB/src/com/company/users/User.java
+++ MovieDB/src/com/company/users/User.java
@@ -1,4 +1,9 @@
 package com.company.users;
 
 public class User {
+    protected String uName;
+    protected String fName;
+    protected String lName;
+    protected String password;
+    protected String email;
 }
