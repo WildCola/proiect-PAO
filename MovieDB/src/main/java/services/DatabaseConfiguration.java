a1d6a8ee4a0ac0bdc545c5f520dab418caa4af93
Placintescu Stefan <stefanplaci@gmail.com>
2022-05-24 19:46:44 +0300

    Update DatabaseConfiguration.java

--- MovieDB/src/main/java/services/DatabaseConfiguration.java
+++ MovieDB/src/main/java/services/DatabaseConfiguration.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfiguration {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/moviedb";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/PAO";
     private static final String USER = "root";
-    private static final String PASSWORD = "arsene2009";
+    private static final String PASSWORD = "root";
     private static Connection databaseConnection;
 
     private DatabaseConfiguration() {
